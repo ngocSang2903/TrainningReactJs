73174adf19779ffcf2346d6fcc58ec716466fb0a
NGOCSANG\ASUS <Kevynsang292003@gmail.com>
2023-07-16 12:08:58 +0700

    update

--- Base_Reactjs/src/components/DisplayInfo.js
+++ Base_Reactjs/src/components/DisplayInfo.js
@@ -2,23 +2,41 @@ import React from "react";
 
 class DisplayInfo extends React.Component{
 
+    state={
+        isShowListUser:false
+    }
+
+    handleShowhide(){
+        this.setState({
+            isShowListUser:!this.state.isShowListUser
+        })
+    }
+
     render(){
         const {listUsers}=this.props;
         console.log(listUsers)
         return(
             <div>
-                {listUsers.map((user)=>{
-                    return(
-                        <div key={user.id}>
-                            <div>My nam is {user.name}</div>
-                            <div>My age is {user.age}</div>
-                            <hr></hr>
-                        </div>
-                    )
-                })}
-                
-                
+                <div>
+                    <span onClick={()=>{this.handleShowhide()}}>
+                        {this.state.isShowListUser===true?"An":"Hien"}
+                    </span>
+                </div>
+                {this.state.isShowListUser &&
+                <div>
+                    {listUsers.map((user)=>{
+                        return(
+                            <div key={user.id} className={+user.age >18 ? "green" : "red"}>
+                                <div>My nam is {user.name}</div>
+                                <div>My age is {user.age}</div>
+                                <hr></hr>
+                            </div>
+                        )
+                    })}
+                </div>
+                }
             </div>
+
         )
     }
 }
--- Base_Reactjs/src/components/MyComponent.js
+++ Base_Reactjs/src/components/MyComponent.js
@@ -7,16 +7,14 @@ class MyCoponent extends React.Component{
 
     state={
         listUsers:[
-            {id:1,name:"sang123", age:"100"},
-            {id:2,name:"huy", age:"100"},
-            {id:3,name:"sang1van23", age:"100"},
-            {id:4,name:"sangnam123", age:"100"}
+            {id:1,name:"sang123", age:"12"},
+            {id:2,name:"huy", age:"67"},
+            {id:3,name:"sang1van23", age:"23"},
+            {id:4,name:"sangnam123", age:"67"}
         ]
     }
 
     render(){
-
-        const myList=["a","b"]
         return(
             <div>
             <UserInfo></UserInfo><br></br>
